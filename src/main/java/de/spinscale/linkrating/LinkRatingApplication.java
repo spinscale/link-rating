fbfc1687cff99c57a62222eb4c1c06f806f64332
Alexander Reelsen <alexander@reelsen.net>
2020-04-27 15:42:38 +0200

    Add Elastic APM

--- src/main/java/de/spinscale/linkrating/LinkRatingApplication.java
+++ src/main/java/de/spinscale/linkrating/LinkRatingApplication.java
@@ -1,5 +1,6 @@
 package de.spinscale.linkrating;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LinkRatingApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(LinkRatingApplication.class, args);
 	}
 }
